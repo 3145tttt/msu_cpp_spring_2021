8cd71dd1e75160759a65e4292e6ea08f95e65c7b
3145tttt <61930551+3145tttt@users.noreply.github.com>
2021-03-21 21:14:02 +0300

    Update allocator.hpp

--- 01/allocator.hpp
+++ 01/allocator.hpp
@@ -6,12 +6,12 @@ class Allocator
 {
 private:
     size_t maxAllocSize = 0;
-	size_t offset = 0;
-	char* ptr = nullptr;
+    size_t offset = 0;
+    char* ptr = nullptr;
 public:
-	void makeAllocator(size_t maxSize);
+    void makeAllocator(size_t maxSize);
     char* alloc(size_t size);
     void reset();
-	size_t getOffset();
-	~Allocator();
+    size_t getOffset();
+    ~Allocator();
 };
